d9bcf4e48055ea05aa9404b93bc9109208aaad28
Carina <106066035+carinadiesel-dev@users.noreply.github.com>
2023-11-14 11:22:10 +0200

    Changed theme settings to dark mode as default

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -5,10 +7,19 @@ import App from "./App.tsx";
 import "./index.css";
 BrowserRouter;
 
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
