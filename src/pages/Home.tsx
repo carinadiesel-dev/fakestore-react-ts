2ba88b43ca096f9de563c8f9db9e6005789f0e54
Carina <106066035+carinadiesel-dev@users.noreply.github.com>
2023-11-20 13:00:44 +0200

    Home page UI desktop version completed

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Store } from "@mui/icons-material";
+import { Box, Button, Typography, useTheme } from "@mui/material";
 import GiftsImg from "../assets/Gifts.png";
 import heroImg from "../assets/hero-image.jpg";
-import CartDrawer from "../components/CartDrawer";
 
 const HomePage = () => {
+  const theme = useTheme();
   return (
     <Box sx={{ display: "flex", overflow: "hidden", height: "55rem" }}>
       <Box
@@ -20,32 +21,44 @@ const HomePage = () => {
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
-          gap: 6,
-          backgroundColor: "#e4dcd3",
-          paddingX: 8,
-          paddingY: 12,
-          justifyContent: "space-between",
+          backgroundColor: theme.palette.success.main,
+          padding: 8,
+          justifyContent: "space-around",
         }}
       >
         <Typography
           sx={{
-            color: "#A74737",
+            color: theme.palette.error.main,
             fontFamily: "fantasy",
-
-            fontSize: "4.5rem",
+            fontSize: "4.3rem",
             lineHeight: "5.5rem",
           }}
         >
           The perfect gift when nothing else will do.
         </Typography>
-        <Typography variant="h4" sx={{ color: "#A74737" }}>
+        <Typography
+          variant="h5"
+          sx={{ fontSize: "1.8rem", color: theme.palette.error.main }}
+        >
           Browse our range of products for the best holiday season deals,
           delivered right to your door
         </Typography>
-        <Box sx={{ display: "flex", py: 2, gap: 8 }}>
-          <Button sx={{ color: "#A74737" }}> Products</Button>
-          <CartDrawer />
-        </Box>
+        <Button
+          endIcon={<Store />}
+          sx={{
+            backgroundColor: theme.palette.error.main,
+            color: "white",
+            fontWeight: "bold",
+            px: 4,
+            py: 1.5,
+
+            "&:hover": {
+              backgroundColor: theme.palette.primary.main,
+            },
+          }}
+        >
+          Shop Now
+        </Button>
         <Box>
           <img src={GiftsImg} alt="" height={200} />
         </Box>
